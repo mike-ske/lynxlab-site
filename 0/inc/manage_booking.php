b90345e5abc545023612a6d75728745d1d09964b
Alumona Micah <micahalumona@gmail.com>
2021-09-13 11:13:54 +0100

    Added new codes

--- 0/inc/manage_booking.php
+++ 0/inc/manage_booking.php
@@ -39,7 +39,7 @@ if (isset($_POST['send']))
          $mail = new PHPMailer();
 
          //Server settings
-        //  $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
+         $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
          $mail->isSMTP();                                            // Send using SMTP
          $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
          $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
@@ -47,7 +47,7 @@ if (isset($_POST['send']))
          $mail->Password   = "thinksoft2021";                               // SMTP password
          $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
          $mail->Port       = 465;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
- 
+  
          //Recipients
          $mail->setFrom('info@lynxlaboratories.com.ng', 'Lynx Support Service');
          $mail->addAddress($email, $phone);     // Add a recipient
