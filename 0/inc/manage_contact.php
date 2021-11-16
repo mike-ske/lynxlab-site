8e875ab8d2d13c75ed471042e0052ce4c9a35d5e
mike-ske <66280654+mike-ske@users.noreply.github.com>
2021-11-16 22:50:07 +0100

    Ok

--- 0/inc/manage_contact.php
+++ 0/inc/manage_contact.php
@@ -36,12 +36,12 @@ if (isset($_POST['send']))
          $mail = new PHPMailer();
 
          //Server settings
-         $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
+        // $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
          $mail->isSMTP();                                            // Send using SMTP
          $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
          $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-         $mail->Username   = "thinksoftcreative@gmail.com";                     // SMTP username
-         $mail->Password   = "thinksoft2021";                               // SMTP password
+         $mail->Username   = "Lynxdna1@gmail.com";                     // SMTP username
+         $mail->Password   = "lynxdna2017";                               // SMTP password
          $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
          $mail->Port       = 465;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
  
