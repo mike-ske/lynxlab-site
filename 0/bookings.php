87db0f2bf0f8bb79e13d75e72216e16ea27b1b9f
mike-ske <66280654+mike-ske@users.noreply.github.com>
2022-01-18 12:52:01 +0100

    Update bookings.php

--- 0/bookings.php
+++ 0/bookings.php
@@ -1,19 +1,6 @@
 <?php require 'header.php' ?>
 <?php require 'nav.php' ?>
 
-
-<!-- ***** Preloader Start ***** -->
-<div id="preloader">
-    <div class="jumper">
-        <div></div>
-        <div></div>
-        <div></div>
-    </div>
-</div>  
-<!-- ***** Preloader End ***** -->
-
-
-
 <!-- BOOKINGS SECTION -->
 <section class="bg_main" id="books_centres">
     <div class="fg_center co_ctre">
